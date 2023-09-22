643f82598be50740a2652a5e277fab82e46030ee
cgh88 <cgh1704@gmail.com>
2023-09-22 13:34:49 +0900

    init2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Streamlit Test")
 st.write("hello world")
+st.header('This is a header with a divider', divider='rainbow')
+st.header('_Streamlit_ is :blue[cool] :sunglasses:')
 st.write("""
 # MarkDown
 > comment
